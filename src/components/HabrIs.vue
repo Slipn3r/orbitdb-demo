350b6f3df4e902012c0549c3df9128bd06d7f5b0
Slipn3r <dep.deep@ya.ru>
2021-02-05 00:21:56 +0300

    fix pubsub issue

--- src/components/HabrIs.vue
+++ src/components/HabrIs.vue
@@ -23,7 +23,6 @@ export default {
       },
       lastUpdated: '',
       db: false,
-      //address: ''
       address: '/orbitdb/zdpuB3UcoWfRJCEeZ7rMsw3CuLjK25ahuTG1KCs9CW2b1HVsV/habr-is'
     }
   },
@@ -34,7 +33,21 @@ export default {
       this.updateDatabase()
     },
     run: async function () {
-      const ipfsOptions = { repo: './ipfs'}
+      const ipfsOptions = {
+        repo: './ipfs',
+        EXPERIMENTAL: {
+          pubsub: true,
+        },
+        config: {
+          Addresses: {
+            Swarm: [
+              '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+              '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+              '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/'
+            ]
+          },
+        }
+      }
       const ipfs = await IPFS.create(ipfsOptions)
       const orbitdb = await OrbitDB.createInstance(ipfs)
       if (!this.address) {
@@ -45,48 +58,51 @@ export default {
     },
     createDatabase: async function (orbitdb) {
       try {
-        this.db = await orbitdb.open('habr-is', {
-          create: true, 
-          overwrite: true,
-          localOnly: false,
-          type: 'keyvalue',
+        const options = {
+          EXPERIMENTAL: {
+            pubsub: true,
+          },
           accessController: {
-            write: ['*'],
+            write: ['*']
           }
-        })
+        }
+
+        this.db = await orbitdb.keyvalue('first-database', options)
         this.address = this.db.address.toString()
         console.log('created database at ' + this.address)
-        await this.db.put('value', 'торт')
-        await this.db.put('lastUpdated', new Date().toLocaleString())
-        this.readDatabase()
+        await this.db.put('value', 'торт', { pin: true })
+        await this.db.put('lastUpdated', new Date().toLocaleString(), { pin: true })
+        this.getData()
       } catch (e) {
         console.error(e)
       }
     },
     openDatabase: async function (orbitdb) {
       try {
-        this.db = await orbitdb.keyvalue(this.address, { sync: true })
+        this.db = await orbitdb.keyvalue(this.address)
         console.log('opened database at ' + this.address)
         console.log(this.db)
-        this.readDatabase()
+        this.getData()
       } catch (e) {
         console.error(e)
       }
     },
-    readDatabase() {
-      try {
-        const value = this.db.get('value')
-        this.active.name = value
-        const lastUpdated = this.db.get('lastUpdated')
-        this.lastUpdated = lastUpdated
-      } catch (e) {
-        console.error(e)
-      }
+    getData() {
+      setInterval(() => {
+        try {
+          const value = this.db.get('value')
+          this.active.name = value
+          const lastUpdated = this.db.get('lastUpdated')
+          this.lastUpdated = lastUpdated
+        } catch (e) {
+          console.error(e)
+        }
+      }, 3000);
     },
     updateDatabase: async function () {
       try {
-        await this.db.set('value', this.active.name)
-        await this.db.set('lastUpdated', this.lastUpdated)
+        await this.db.put('value', this.active.name, { pin: true })
+        await this.db.put('lastUpdated', this.lastUpdated, { pin: true })
       } catch (e) {
         console.error(e)
       }
